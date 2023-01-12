9a902e7adc40d7b5e346d9c693bfbf8357273e76
star-sil <kimse9811@naver.com>
2023-01-12 17:26:48 +0900

    init project

---
+++ src/main/java/com/yogeunbang/ygbbackend/YgbBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.yogeunbang.ygbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgbBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgbBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yogeunbang/ygbbackend/YgbBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogeunbang.ygbbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgbBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
