db403514c550334fb32507dacc4b431a7f5f6a1a
star-sil <kimse9811@naver.com>
2023-02-04 20:57:51 +0900

    test: 관광지 조회 테스트 추가

--- src/test/java/com/yogeunbang/ygbbackend/accommodation/AccommodationControllerTest.java
+++ src/test/java/com/yogeunbang/ygbbackend/accommodation/AccommodationControllerTest.java
@@ -1,5 +1,6 @@
 package com.yogeunbang.ygbbackend.accommodation;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -7,7 +8,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.yogeunbang.ygbbackend.accommodation.dto.RegionDto;
+import com.yogeunbang.ygbbackend.accommodation.dto.SpotDto;
 import com.yogeunbang.ygbbackend.accommodation.entity.Region;
+import com.yogeunbang.ygbbackend.accommodation.entity.Spot;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,9 +43,26 @@ public class AccommodationControllerTest {
         mvc.perform(get("/regions"))
             .andExpect(status().isOk())
             .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("$.[0].id").value("1"))
             .andExpect(jsonPath("$.[0].name").value("제주도"));
 
     }
 
+    @Test
+    public void 관광지_조회_테스트() throws Exception {
+
+        //given
+        given(accommodationService.findSpots(any(Long.class)))
+            .willReturn(Arrays.asList(new SpotDto(Spot.builder()
+                .name("성산일출봉")
+                .car(Boolean.TRUE)
+                .description("관광지 설명")
+                .image("이미지 경로")
+                .build())));
+
+        //when & then
+        mvc.perform(get("/regions/1/spots"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.[0].name").value("성산일출봉"));
+    }
 }
--- src/test/java/com/yogeunbang/ygbbackend/accommodation/AccommodationServiceTest.java
+++ src/test/java/com/yogeunbang/ygbbackend/accommodation/AccommodationServiceTest.java
@@ -1,13 +1,15 @@
 package com.yogeunbang.ygbbackend.accommodation;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 import com.yogeunbang.ygbbackend.accommodation.dto.RegionDto;
+import com.yogeunbang.ygbbackend.accommodation.dto.SpotDto;
 import com.yogeunbang.ygbbackend.accommodation.entity.Region;
+import com.yogeunbang.ygbbackend.accommodation.entity.Spot;
 import com.yogeunbang.ygbbackend.accommodation.repo.RegionRepo;
+import com.yogeunbang.ygbbackend.accommodation.repo.SpotRepo;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,6 +25,8 @@ class AccommodationServiceTest {
 
     @Mock
     private RegionRepo regionRepo;
+    @Mock
+    private SpotRepo spotRepo;
 
     @Test
     public void 지역_조회_테스트() {
@@ -40,5 +44,23 @@ class AccommodationServiceTest {
         //then
         assertThat(regions.get(0).getName()).isEqualTo("제주도");
         assertThat(regions.get(1).getName()).isEqualTo("부산");
+    }
+
+    @Test
+    public void 관광지_조회_테스트() {
+
+        //given
+        given(spotRepo.findByRegion_Id(any(Long.class)))
+            .willReturn(List.of(
+                Spot.builder().name("성산일출봉").build(),
+                Spot.builder().name("중문 관광단지").build())
+            );
+
+        //when
+        List<SpotDto> spots = accommodationService.findSpots(1L);
+
+        //then
+        assertThat(spots.get(0).getName()).isEqualTo("성산일출봉");
+        assertThat(spots.get(1).getName()).isEqualTo("중문 관광단지");
     }
 }
